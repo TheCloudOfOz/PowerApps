19262a74598bcc618da7eabe5a309b1504159400
Simon Ozturk <ozturksuleyman@msn.com>
2021-08-20 13:51:56 -0700

    Add files via upload

---
+++ Connect.ps1
@@ -0,0 +1,26 @@
+using namespace Microsoft.IdentityModel.Clients.ActiveDirectory
+Add-Type -Path .\Microsoft.IdentityModel.Clients.ActiveDirectory.dll
+
+$TenantId = ''
+$ClientId = ''
+$ClientSecret = ''
+$Resource = 'https://<Organization>.crm.dynamics.com'
+$TenantUrl = "https://login.microsoftonline.com/$TenantId"
+
+$Context = [AuthenticationContext]::new($TenantUrl)
+$ClientCredential = [ClientCredential]::new($ClientId, $ClientSecret)
+$Result = $Context.AcquireTokenAsync($Resource, $ClientCredential).Result
+$Result.AccessToken
+
+$REST = @{
+    Uri = 'https://<Organization>.api.crm.dynamics.com/api/data/v9.2/accounts?$select=name'
+    Method = 'Get'
+    ContentType = 'application/json'
+    Headers = @{
+        ContentType = 'application/json'
+        Authorization = "Bearer $($Result.AccessToken)"
+    }
+}
+(Invoke-RestMethod @REST).value | Format-Table
+
+Invoke-RestMethod -Method Delete -Uri "https://jsonplaceholder.typicode.com/posts/1"
